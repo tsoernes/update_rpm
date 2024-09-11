34be28d50bcc12b44d97afdae3826a863a0bf729
Torstein Sørnes <t.soernes@gmail.com>
2024-09-11 20:42:56 +0200

    begin integrating all endpoints into a single file

--- update_rpm.py
+++ update_rpm.py
@@ -6,22 +6,89 @@ Script to install or update an RPM package from a link
 import argparse
 import shlex
 import subprocess
+import sys
 import tempfile
 from pathlib import Path
 
 import requests
 from packaging.version import Version
+from version_utils import rpm
+
+endpoints = ["url", "json", "github"]
+
+presets = {
+    "plex": {
+        "endpoint": "json",
+        "json_url": "https://plex.tv/api/downloads/5.json",
+        "json_selector": "['computer']['Linux']['releases'][? distro == 'redhat' & build = 'linux-x86_64']['url']",
+    },
+    "lapce": {
+        "endpoint": "github",
+        "repo": "lapce/lapce",
+        "file_selector": ".rpm",
+    },
+    "thorium": {
+        "endpoint": "github",
+        "repo": "Alex313031/thorium",
+        "file_selector": "AVX2.rpm",
+    },
+    "azuredatastudio": {
+        "endpoint": "url",
+        "url": "https://azuredatastudio-update.azurewebsites.net/latest/linux-rpm-x64/stable",
+    },
+}
+
 
 parser = argparse.ArgumentParser(
     prog="update-rpm",
     description="Fetch the latest release RPM of a package from a download link. Install it.",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
-parser.add_argument("url", help="RPM download link", type=str)
+subparsers = parser.add_subparsers(
+    "endpoint",
+    help="The type of endpoint you want to install from. Or choose from a given preset.",
+    type=str,
+    choices=endpoints + ["preset"],
+    dest="endpoint",
+)
+
+url_parser_help = "RPM direct download URL"
+url_parser = subparsers.add_parser("url", help=url_parser_help)
+url_parser.add_argument("url", help=url_parser_help, type=str)
+
+github_parser_help = "Download RPM from Github releases"
+github_parser = subparsers.add_parser("github", help=github_parser_help)
+github_parser.add_argument(
+    "repo",
+    help="{owner}/{repository} Owner and repository name. Example: 'lapce/lapce'",
+    type=str,
+)
+
+json_parser_help = "URL to JSON that contains download URL for the RPM file"
+json_parser = subparsers.add_parser("json", help=json_parser_help)
+json_parser.add_argument("json_url", help=json_parser_help, type=str)
+json_parser.add_argument(
+    "-s",
+    "--json_selector",
+    help="JSON Selector. For syntax see https://github.com/h2non/jsonpath-ng?tab=readme-ov-file#",
+    type=str,
+)
+
+preset_parser = subparsers.add_parser("preset", help="Choose from a given preset")
+preset_parser.add_argument(
+    "preset", help=str(presets), choices=presets.keys(), type=str
+)
+
 parser.add_argument(
     "-d",
+    "--directory",
+    help="Directory to store the RPM in",
+    default=tempfile.gettempdir(),
+)
+parser.add_argument(
+    "-r",
     "--redownload",
-    help="Whether to redownload the RPM if it is already downloaded",
+    help="Force download the RPM if it is already downloaded",
     default=False,
     action="store_true",
 )
@@ -34,14 +101,48 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
+endpoint = args.endpoint
 redownload = args.redownload
 reinstall = args.reinstall
+directory = args.directory
+
+if endpoint == "preset":
+    args = parser.parse_args(presets[args.preset])
+    endpoint = args.endpoint
+
+if endpoint == "github":
+    try:
+        owner, repo = args.repo.split("/")[-2:]
+    except Exception as e:
+        print(e)
+        print("Invalid owner + repo: ", args.repo)
+        sys.exit(1)
+    json_url = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
+    file_selector = args.file_selector
+
+    resp = requests.get(json_url)
+    js = resp.json()
+    version = js["tag_name"][1:]
+    try:
+        release = next(x for x in js["assets"] if file_selector in x["name"])
+    except StopIteration:
+        names = "\n".join([x["name"] for x in js["assets"]])
+        print(f"{file_selector=} not found. Available options:\n{names}")
+        sys.exit(1)
+    url = release["browser_download_url"]
+    fname = release["name"]
+
+elif endpoint == "json":
+    json_url = args.json_url
+
+if endpoint in ("json", "github"):
+    pass
 
 # Get file name after redirects. We assume that the package version is included in the file name.
 resp = requests.head(args.url, allow_redirects=True)
 url = resp.url
 fname = url.split("/")[-1]
-path = Path(tempfile.gettempdir()) / fname
+path = Path(directory / fname)
 
 # Check if file exists to avoid redownload
 if path.exists():
@@ -51,6 +152,48 @@ if path.exists():
         download = False
 else:
     download = True
+
+
+# Attempt to check if package is already installed, and if so, its version.
+# We must infer package name and version from file name which might not always work; if e.g. the package is named "xx.rpm".
+# If the package is already installed, we might break out of the program early to avoid downloading the file again.
+try:
+    inferred_package_name = rpm.package(fname).name
+except IndexError:
+    # Could not infer package name
+    pass
+else:
+    # Find the installed RPM package name and version
+    installed_versions = subprocess.run(
+        "rpm -q " + str(inferred_package_name),
+        shell=True,
+        capture_output=True,
+        text=True,
+    ).stdout.splitlines()
+    if len(installed_versions) == 1:
+        installed_version = installed_versions[0]
+        if not installed_version.endswith(
+            "not installed"
+        ) and not installed_version.endswith(
+            "NO KEY"
+        ):  # xx
+            version_cmp = rpm.compare_packages(fname, installed_version)
+            if version_cmp < 0:
+                print(
+                    f"Detected that {installed_version} is installed, but {fname} is newer"
+                )
+            elif version_cmp == 0:
+                print(
+                    f"Detected that {installed_version} is installed, but {fname} is same"
+                )
+                if not reinstall:
+                    sys.exit(0)
+            else:
+                print(
+                    f"Detected that {installed_version} is installed, but {fname} is older"
+                )
+                sys.exit(0)
+
 
 if download:
     print(f"Downloading {url} to {path}")
@@ -79,25 +222,26 @@ installed_versions = cmd_result.stdout.splitlines()
 install = True
 if len(installed_versions) == 1:
     installed_version = installed_versions[0]
-    print(f"{package_name} {installed_version} already installed")
-    rpm_version = subprocess.run(
-        'rpm -qp --queryformat "%{VERSION}" ' + str(path),
-        shell=True,
-        capture_output=True,
-        text=True,
-    ).stdout
-    if Version(installed_version) < Version(rpm_version):
-        install = True
-    elif Version(installed_version) == Version(rpm_version):
-        if reinstall:
+    if not installed_version.endswith("not installed"):
+        print(f"{package_name} {installed_version} already installed")
+        rpm_version = subprocess.run(
+            'rpm -qp --queryformat "%{VERSION}" ' + str(path),
+            shell=True,
+            capture_output=True,
+            text=True,
+        ).stdout
+        if Version(installed_version) < Version(rpm_version):
             install = True
+        elif Version(installed_version) == Version(rpm_version):
+            if reinstall:
+                install = True
+            else:
+                print(f"Version {installed_version} already installed; not installing.")
+                install = False
         else:
-            print(f"Version {installed_version} already installed; not installing.")
-            install = False
-    else:
-        print(
-            f"Installed version {installed_version} newer than downloaded version {rpm_version}; not installing."
-        )
+            print(
+                f"Installed version {installed_version} newer than downloaded version {rpm_version}; not installing."
+            )
 
 if install:
     cmd = ["sudo", "-S", "dnf", "install", "--assumeyes", str(path)]
